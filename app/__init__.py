0a91fee85753684aa4d15e96c4dc71da8f9e837f
hannahtan96 <hannahtan96@gmail.com>
2022-11-01 10:12:06 -0700

    added put and patch functions for breakfast

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# setting up initial objects for us to use later in app
+db = SQLAlchemy()
+migrate = Migrate() 
 
 def create_app():
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/breakfasts_development'
+    app.config['SQUALCHEMY_ECHO'] = True
+    
+    # connecting app to classes, which manage sqlalchemy and migrations
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.models.breakfast import Breakfast
+
+    from .routes.breakfast import breakfast_bp # register the breakfast blueprint / endpoint
+    app.register_blueprint(breakfast_bp)
 
     return app
---
+++ app/models/breakfast.py
@@ -0,0 +1,15 @@
+from app import db
+
+class Breakfast(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    rating = db.Column(db.Float)
+    prep_time = db.Column(db.Integer)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "rating": self.rating,
+            "prep_time": self.prep_time
+        }
