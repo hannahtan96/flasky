2f697dc2768ad3afd504c5322aec3861e3447db3
hannahtan96 <hannahtan96@gmail.com>
2022-11-10 21:09:51 -0800

    changed menu_id to be optional

--- app/models/breakfast.py
+++ app/models/breakfast.py
@@ -5,7 +5,7 @@ class Breakfast(db.Model):
     name = db.Column(db.String)
     rating = db.Column(db.Float)
     prep_time = db.Column(db.Integer)
-    menu_id = db.Column(db.Integer, db.ForeignKey('menu.id'))
+    menu_id = db.Column(db.Integer, db.ForeignKey('menu.id'), default=None)
     menu = db.relationship('Menu', back_populates='breakfast_items')
 
     def to_dict(self):
@@ -22,9 +22,10 @@ class Breakfast(db.Model):
 
     @classmethod
     def from_dict(cls, breakfast_dict):
+
         return cls(
             name=breakfast_dict["name"],
             rating=breakfast_dict["rating"],
             prep_time=breakfast_dict["prep_time"],
-            menu_id=breakfast_dict["menu_id"]
+            menu_id=breakfast_dict.get("menu_id")
         )
